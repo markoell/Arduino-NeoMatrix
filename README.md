5091aef1f9139480c487ebf3a1bcdcb487d3917f
Marco Köllner <it@mk-swe.de>
2017-10-22 11:12:46 +0200

    Initial source files

---
+++ README.md
@@ -0,0 +1,4 @@
+Arduino Time Mashine
+======================
+
+Arduino -Projekt zur Ansteuerung einer LED Matrix für die Rebläuse
---
+++ src/sketch_oct16b.ino
@@ -0,0 +1,124 @@
+// Simple NeoPixel test.  Lights just a few pixels at a time so a
+// 1m strip can safely be powered from Arduino 5V pin.  Arduino
+// may nonetheless hiccup when LEDs are first connected and not
+// accept code.  So upload code first, unplug USB, connect pixels
+// to GND FIRST, then +5V and digital pin 6, then re-plug USB.
+// A working strip will show a few pixels moving down the line,
+// cycling between red, green and blue.  If you get no response,
+// might be connected to wrong end of strip (the end wires, if
+// any, are no indication -- look instead for the data direction
+// arrows printed on the strip).
+ 
+
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+ 
+#define PIN      6
+#define N_LEDS 448
+#define NEO_MATRIX_HIGHT 16
+#define NEO_MATRIX_WIDTH 28
+ 
+//Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+
+
+// MATRIX DECLARATION:
+// Parameter 1 = width of NeoPixel matrix
+// Parameter 2 = height of matrix
+// Parameter 3 = pin number (most are valid)
+// Parameter 4 = matrix layout flags, add together as needed:
+//   NEO_MATRIX_TOP, NEO_MATRIX_BOTTOM, NEO_MATRIX_LEFT, NEO_MATRIX_RIGHT:
+//     Position of the FIRST LED in the matrix; pick two, e.g.
+//     NEO_MATRIX_TOP + NEO_MATRIX_LEFT for the top-left corner.
+//   NEO_MATRIX_ROWS, NEO_MATRIX_COLUMNS: LEDs are arranged in horizontal
+//     rows or in vertical columns, respectively; pick one or the other.
+//   NEO_MATRIX_PROGRESSIVE, NEO_MATRIX_ZIGZAG: all rows/columns proceed
+//     in the same order, or alternate lines reverse direction; pick one.
+//   See example below for these values in action.
+// Parameter 5 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_GRBW    Pixels are wired for GRBW bitstream (RGB+W NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+
+
+// Example for NeoPixel Shield.  In this application we'd like to use it
+// as a 5x8 tall matrix, with the USB port positioned at the top of the
+// Arduino.  When held that way, the first pixel is at the top right, and
+// lines are arranged in columns, progressive order.  The shield uses
+// 800 KHz (v2) pixels that expect GRB color data.
+
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix (NEO_MATRIX_WIDTH, NEO_MATRIX_HIGHT, PIN, NEO_MATRIX_BOTTOM + NEO_MATRIX_RIGHT + NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG,NEO_GRB + NEO_KHZ800);
+
+const uint16_t colors[] = {
+  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) };
+
+
+int maxCounter = 50; //Dauer / Wiederholungen Krisseln
+
+//int[] jahre = 2017, 1539,...
+
+void setup() {
+  //strip.begin();
+
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(40);
+  matrix.setTextColor(matrix.Color(255,0,0));
+}
+ int test = 0;
+
+ int x    = matrix.width();
+ int pass = 0;
+
+void loop() {
+
+// if(test == 1)
+// return;
+ //if button of
+  //return
+
+//  for(int i = 0 ; i < maxCounter; i++){
+//    printRandom();
+//    delay(50);
+//  }
+
+// Schreibe Zahl aus Array
+  printJear();
+  test = 1;
+  
+  //delay(50);
+}
+
+static int randomBrightnessValue(){
+    return (rand() % 5) * 50; 
+}
+
+void printRandom(){
+  //for (int i=0; i < strip.numPixels(); i++){
+    //strip.setPixelColor(i, strip.Color(randomBrightnessValue(),0,0));
+  //}
+  //strip.show();
+}
+
+void printJear(){
+  matrix.fillScreen(0);
+  matrix.setCursor(x, 0);
+  matrix.print(F("Howdy"));
+  matrix.show();
+  delay(100);
+}
+
+void clr(){
+ //for (int i=0; i < strip.numPixels(); i++){
+    //strip.setPixelColor(i, strip.Color(0,0,0));
+  //}
+  //strip.show(); 
+}
